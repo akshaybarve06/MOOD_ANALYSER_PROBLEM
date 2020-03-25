fa683b63330073175402ccbdd3e6ec30e5bbb0c9
Akshay Barve <akshaybarve06@gmail.com>
2020-03-25 20:07:18 +0530

    Added: Repeat TC 1.2 Analyse Any Mood

--- src/Test/Java/MoodAnalyserTest.java
+++ src/Test/Java/MoodAnalyserTest.java
@@ -11,5 +11,9 @@ public class MoodAnalyserTest {
         MoodAnalyser object=new MoodAnalyser("I'm in sad mood");
         Assert.assertEquals("SAD",object.moodAnalyseMethod());
     }
-
+    @Test
+    public void message_whenResponseHappy(){
+        MoodAnalyser object=new MoodAnalyser("I'm in any mood");
+        Assert.assertEquals("HAPPY",object.moodAnalyseMethod());
+    }
 }
